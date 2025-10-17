fffc409d02f18eb5b9a1748090af0679cca67cb3
KHUSHI8924 <khushidriven@gmail.com>
2025-10-17 10:15:57 +0530

    adding comments

--- transcribing.py
+++ transcribing.py
@@ -1,8 +1,8 @@
-t=input("Enter the nucleotide of Dna for transcription:".upper()) 
-u=""
+t=input("Enter the nucleotide of DNA for transcription:".upper())    #input of DNA as t
+u=""                          #output of RNA as u
 for base in t:
     if base=='T':
-        u+='U'
+        u+='U'                #changing base from 'T' to 'U' in RNA 
     else:
         u+=base
-print(u.upper())
+print(u.upper())              #printing output 
